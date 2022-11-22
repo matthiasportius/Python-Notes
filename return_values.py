93fc5512844903349cccfcde659fa4e66585c267
Matthias Portius <Matthias.Portiusmx.de>
2022-11-22 23:45:01 +0100

    added generators + walruses

--- return_values.py
+++ return_values.py
@@ -1,1 +1,23 @@
-# return, yield, := (directly assignes?)
+# yield only returns one value at a time
+def main():
+    n = int(input("Enter a number: "))
+    # implicitly calls __next__ of generator
+    for s in count(n):
+        print(s)
+
+    walrus()
+
+
+def count(n):
+    for i in range(n):
+        yield "🐑" * i
+
+
+# assign variables within expressions
+def walrus():
+    print(w := "walrus")
+    print("The variable can be used again, see:", w)
+
+
+if __name__ == "__main__":
+    main()
